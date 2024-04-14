bc7e1437437b57109a4c54821434767463d70f30
nicog3314 <nic.gonzalez1001@gmail.com>
2024-04-13 23:15:50 -0400

    April 13, 2024

--- Daily Dose/Views/App Screen/Meal Screens/BulkMealsView.swift
+++ Daily Dose/Views/App Screen/Meal Screens/BulkMealsView.swift
@@ -7,50 +7,58 @@
 
 import SwiftUI
 
-
-//let testData = [
-//    Meals(id: NSUUID().uuidString, name: "Test", ingredient: ["1", "2", "3", "yum"], calories: 100, protein: 30, carbs: 20, fats: 10, imageURL: "test.org"),
-//    Meals(id: NSUUID().uuidString, name: "Test", ingredient: ["1", "2", "3", "ew"], calories: 100, protein: 30, carbs: 20, fats: 10, imageURL: "test.com"),
-//    Meals(id: NSUUID().uuidString, name: "Test", ingredient: ["1", "2", "3", "4", "5"], calories: 100, protein: 30, carbs: 20, fats: 10, imageURL: "test.org")
-//]
-
 struct BulkMealsView: View {
-    
-    
-//    init(){
-//        UINavigationBar.appearance().backgroundColor = .opaqueSeparator
-//    }
-    
-    
     @ObservedObject private var viewModel = MealsViewModel()
-//    var meals = testData
     var body: some View {
         
-        NavigationView{
-            List(viewModel.meals){
-                meal in
-                VStack(alignment: .leading){
-                    Text(meal.name)
-                        .font(.headline)
-                    Text(meal.ingredient.joined())
-                        .font(.subheadline)
-                    Text(meal.calories.formatted())
-                        .font(.subheadline)
-                    Text(meal.protein.formatted())
-                        .font(.subheadline)
-                    Text(meal.carbs.formatted())
-                        .font(.subheadline)
-                    Text(meal.fats.formatted())
-                        .font(.subheadline)
+        NavigationView {
+                    List {
+                        ForEach(viewModel.meals) { meal in
+                            DisclosureGroup {
+                                MealInfo(meal: meal)
+                            } label: {
+                                HStack {
+                                    // Image of the meal
+                                    AsyncImage(url: URL(string: meal.imageURL)) { image in
+                                        image
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 75, height: 75)
+                                            .cornerRadius(15)
+                                    } placeholder: {
+                                        ProgressView()
+                                    }
+                                    .frame(width: 75, height: 75)
+                                    .shadow(radius: 10)
+
+                                    // Name of the meal
+                                    Text(meal.name)
+                                        .font(.headline)
+                                }
+                            }
+                        }
+                    }
+                    .onAppear {
+                        viewModel.fetchBulkMeals()
+                    }
                 }
             }
-        }
-        .onAppear(){
-            self.viewModel.fetchBulkMeals()
-            }
-        }
 }
 
+
+struct MealInfo: View {
+    let meal: Meals
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+                Text("Ingredients: \(meal.ingredient.joined(separator: ", "))")
+                   Text("Calories: \(meal.calories)")
+                   Text("Fats: \(meal.fats)g")
+                   Text("Proteins: \(meal.protein)g")
+                   Text("Carbs: \(meal.carbs)g")
+               }
+               .padding(.vertical, 4)
+    }
+}
 #Preview {
     BulkMealsView()
 }
